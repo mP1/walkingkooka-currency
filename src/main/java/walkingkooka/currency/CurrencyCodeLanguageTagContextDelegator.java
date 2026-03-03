106dcb8f658ce0efa2800a175264cbe0eb1ed996
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-03 20:39:59 +1100

    CurrencyCodeLanguageTagContextDelegator

---
+++ src/main/java/walkingkooka/currency/CurrencyCodeLanguageTagContextDelegator.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.currency;
+
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Optional;
+
+public interface CurrencyCodeLanguageTagContextDelegator extends CurrencyCodeLanguageTagContext {
+
+    @Override
+    default Optional<Currency> currencyForCurrencyCode(final String currencyCode) {
+        return this.currencyCodeLanguageTagContext()
+            .currencyForCurrencyCode(currencyCode);
+    }
+
+    @Override
+    default Optional<Locale> localeForLanguageTag(final String languageTag) {
+        return this.currencyCodeLanguageTagContext()
+            .localeForLanguageTag(languageTag);
+    }
+
+    CurrencyCodeLanguageTagContext currencyCodeLanguageTagContext();
+}
