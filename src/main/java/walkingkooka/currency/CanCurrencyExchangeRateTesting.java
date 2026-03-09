df7bb2e308f63fae18c789589718b48b61c09d35
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-09 13:53:45 +1100

    CanCurrencyExchangeRate

---
+++ src/main/java/walkingkooka/currency/CanCurrencyExchangeRateTesting.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.currency;
+
+import walkingkooka.text.printer.TreePrintableTesting;
+
+import java.util.Currency;
+
+public interface CanCurrencyExchangeRateTesting extends TreePrintableTesting {
+
+    default void exchangeRateAndCheck(final CanCurrencyExchangeRate can,
+                                      final Currency from,
+                                      final Currency to,
+                                      final Number expected) {
+        this.checkEquals(
+            expected,
+            can.exchangeRate(
+                from,
+                to
+            ),
+            "exchangeRate " + from.getCurrencyCode() + " to " + to.getCurrencyCode()
+        );
+    }
+}
