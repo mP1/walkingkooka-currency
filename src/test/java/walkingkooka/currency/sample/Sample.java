4be69485440d7f9b43d991d4a7e25bc640451143
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-09 17:52:29 +1100

    Sample.testExchangeRate()

--- src/test/java/walkingkooka/currency/sample/Sample.java
+++ src/test/java/walkingkooka/currency/sample/Sample.java
@@ -1,21 +1,54 @@
-/*
- * Copyright 2026 Miroslav Pokorny (github.com/mP1)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package walkingkooka.currency.sample;
 
+import org.junit.jupiter.api.Test;
+import walkingkooka.currency.ExchangeRate;
+import walkingkooka.props.Properties;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Sample {
+
+    public static void main(final String[] args) {
+        final Sample sample = new Sample();
+        sample.testExchangeRate();
+    }
+
+    @Test
+    public void testExchangeRate() {
+        checkEquals(
+            new BigDecimal("1.1"),
+            ExchangeRate.fromProperties(
+                Properties.parse("AUD-NZD=1.1"),
+                (String text, Boolean invert) -> {
+                    final BigDecimal value = new BigDecimal(text);
+                    return invert ?
+                        BigDecimal.ONE.divide(
+                            value,
+                            2,
+                            BigDecimal.ROUND_HALF_UP
+                        ) :
+                        value;
+                }
+            ).exchangeRate(
+                Currency.getInstance("AUD"),
+                Currency.getInstance("NZD"),
+                Optional.empty()
+            ),
+            "AUD-NZD"
+        );
+    }
+
+    private static void checkEquals(final Object expected,
+                                    final Object actual,
+                                    final String message) {
+        assertEquals(
+            expected,
+            actual,
+            message
+        );
+    }
 }
