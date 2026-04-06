cfc83c2d169b571e0b4d8786605c95986543f0ed
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-06 13:45:20 +1000

    CurrencyCodeSet

---
+++ src/main/java/walkingkooka/currency/CurrencyCodeSet.java
@@ -0,0 +1,174 @@
+package walkingkooka.currency;
+
+import walkingkooka.collect.iterator.Iterators;
+import walkingkooka.collect.set.ImmutableSortedSetDefaults;
+import walkingkooka.collect.set.Sets;
+import walkingkooka.collect.set.SortedSets;
+import walkingkooka.text.CharacterConstant;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.TreePrintable;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Currency;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * An immutable {@link Set} containing unique {@link CurrencyCode currencies}.
+ */
+public final class CurrencyCodeSet extends AbstractSet<CurrencyCode>
+    implements ImmutableSortedSetDefaults<CurrencyCodeSet, CurrencyCode>,
+    TreePrintable {
+
+    /**
+     * An empty {@link CurrencyCodeSet}.
+     */
+    public static final CurrencyCodeSet EMPTY = new CurrencyCodeSet(SortedSets.empty());
+
+    /**
+     * The comma which separates the CSV text representation.
+     */
+    public static final CharacterConstant SEPARATOR = CharacterConstant.COMMA;
+
+    /**
+     * Handy filter that creates a {@link CurrencyCode} for each locale that starts with that given text
+     */
+    public static Set<CurrencyCode> filter(final String startsWith,
+                                           final CurrencyContext context) {
+
+        final Set<CurrencyCode> matched = Sets.ordered();
+
+        for (final Currency currency : context.findByCurrencyText(
+            startsWith,
+            0,
+            Integer.MAX_VALUE
+        )) {
+            final String currencyText = context.currencyText(currency)
+                .orElse(null);
+
+            if (null != currencyText && (CurrencyContexts.CASE_SENSITIVITY.startsWith(currencyText, startsWith) || CurrencyContexts.CASE_SENSITIVITY.equals(currencyText, startsWith))) {
+                matched.add(
+                    CurrencyCode.fromCurrency(currency)
+                );
+            }
+        }
+
+        return matched;
+    }
+
+    /**
+     * Factory that creates {@link CurrencyCodeSet} with the given currencies.
+     */
+    public static CurrencyCodeSet with(final Collection<CurrencyCode> currencies) {
+        return EMPTY.setElements(currencies);
+    }
+
+    private static CurrencyCodeSet withCopy(final SortedSet<CurrencyCode> currencies) {
+        return currencies.isEmpty() ?
+            EMPTY :
+            new CurrencyCodeSet(currencies);
+    }
+
+    private CurrencyCodeSet(final SortedSet<CurrencyCode> currencyCodes) {
+        super();
+        this.currencyCodes = currencyCodes;
+    }
+
+    // ImmutableSortedSet...............................................................................................
+
+    @Override
+    public Iterator<CurrencyCode> iterator() {
+        return Iterators.readOnly(
+            this.currencyCodes.iterator()
+        );
+    }
+
+    @Override
+    public int size() {
+        return this.currencyCodes.size();
+    }
+
+    @Override
+    public Comparator<CurrencyCode> comparator() {
+        return null;
+    }
+
+    @Override
+    public CurrencyCodeSet subSet(final CurrencyCode from,
+                                  final CurrencyCode to) {
+        return withCopy(
+            this.currencyCodes.subSet(
+                from,
+                to
+            )
+        );
+    }
+
+    @Override
+    public CurrencyCodeSet headSet(final CurrencyCode locale) {
+        return withCopy(
+            this.currencyCodes.headSet(locale)
+        );
+    }
+
+    @Override
+    public CurrencyCodeSet tailSet(final CurrencyCode locale) {
+        return withCopy(
+            this.currencyCodes.tailSet(locale)
+        );
+    }
+
+    @Override
+    public CurrencyCode first() {
+        return this.currencyCodes.first();
+    }
+
+    @Override
+    public CurrencyCode last() {
+        return this.currencyCodes.last();
+    }
+
+    @Override
+    public SortedSet<CurrencyCode> toSet() {
+        return new TreeSet<>(this.currencyCodes);
+    }
+
+    @Override
+    public CurrencyCodeSet setElements(final Collection<CurrencyCode> currencyCodes) {
+        final CurrencyCodeSet currencyCodeSet;
+
+        if (currencyCodes instanceof CurrencyCodeSet) {
+            currencyCodeSet = (CurrencyCodeSet) currencyCodes;
+        } else {
+            final TreeSet<CurrencyCode> copy = new TreeSet<>(
+                Objects.requireNonNull(currencyCodes, "currencies")
+            );
+            currencyCodeSet = this.currencyCodes.equals(copy) ?
+                this :
+                withCopy(copy);
+        }
+
+        return currencyCodeSet;
+    }
+
+    private final SortedSet<CurrencyCode> currencyCodes;
+
+    @Override
+    public void elementCheck(final CurrencyCode locale) {
+        Objects.requireNonNull(locale, "locale");
+    }
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        for (final CurrencyCode currencyCode : this) {
+            currencyCode.printTree(printer);
+        }
+    }
+}
---
+++ src/test/java/walkingkooka/currency/CurrencyCodeSetTest.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2026 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.currency;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.set.ImmutableSortedSetTesting;
+import walkingkooka.collect.set.Sets;
+import walkingkooka.collect.set.SortedSets;
+import walkingkooka.text.printer.TreePrintableTesting;
+
+import java.util.Currency;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class CurrencyCodeSetTest implements ImmutableSortedSetTesting<CurrencyCodeSet, CurrencyCode>,
+    TreePrintableTesting {
+
+    private final static CurrencyCode AUD = CurrencyCode.fromCurrency(
+        Currency.getInstance("AUD")
+    );
+
+    private final static CurrencyCode NZD = CurrencyCode.fromCurrency(
+        Currency.getInstance("NZD")
+    );
+
+    @Test
+    public void testWithNullFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> CurrencyCodeSet.with(null)
+        );
+    }
+
+    @Test
+    public void testDeleteBecomesEmpty() {
+        assertSame(
+            CurrencyCodeSet.EMPTY,
+            CurrencyCodeSet.EMPTY.concat(AUD)
+                .delete(AUD)
+        );
+    }
+
+    @Test
+    public void testSetElementsWithCurrencyCodeSet() {
+        final CurrencyCodeSet set = CurrencyCodeSet.with(
+            SortedSets.of(
+                CurrencyCode.fromCurrency(
+                    Currency.getInstance("USD")
+                )
+            )
+        );
+
+        assertSame(
+            set,
+            set.setElements(set)
+        );
+    }
+
+    @Test
+    public void testSetElementsWithCurrencyCodeSet2() {
+        final CurrencyCodeSet set = this.createSet();
+        final CurrencyCodeSet set2 = this.createSet();
+
+        assertSame(
+            set2,
+            set.setElements(
+                set2
+            )
+        );
+    }
+
+    @Override
+    public CurrencyCodeSet createSet() {
+        final SortedSet<CurrencyCode> sortedSet = SortedSets.tree();
+
+        sortedSet.add(AUD);
+        sortedSet.add(NZD);
+
+        return CurrencyCodeSet.with(
+            SortedSets.of(
+                AUD,
+                NZD
+            )
+        );
+    }
+
+    // filter...........................................................................................................
+
+    private final static Currency AUD_CURRENCY = Currency.getInstance("AUD");
+    private final static Currency NZD_CURRENCY = Currency.getInstance("NZD");
+    private final static Currency EUR_CURRENCY = Currency.getInstance("EUR");
+
+    private final static String AUD_TEXT = "Australia Dollar";
+    private final static String NZD_TEXT = "New Zealand Dollar";
+    private final static String EUR_TEXT = "Euro 123";
+
+    private final static CurrencyContext CONTEXT = new FakeCurrencyContext() {
+
+        @Override
+        public Set<Currency> findByCurrencyText(final String text,
+                                                final int offset,
+                                                final int count) {
+            return Sets.of(
+                AUD_CURRENCY,
+                NZD_CURRENCY,
+                EUR_CURRENCY
+            );
+        }
+
+        @Override
+        public Optional<String> currencyText(final Currency currency) {
+            return Optional.ofNullable(
+                AUD_CURRENCY.equals(currency) ?
+                    AUD_TEXT :
+                    NZD_CURRENCY.equals(currency) ?
+                        NZD_TEXT :
+                        EUR_CURRENCY.equals(currency) ?
+                            EUR_TEXT :
+                            null
+            );
+        }
+    };
+
+    @Test
+    public void testFilterMatchesNone() {
+        this.filterAndCheck(
+            "Z",
+            CONTEXT
+        );
+    }
+
+    @Test
+    public void testFilterMatchesSome() {
+        this.filterAndCheck(
+            "Australia",
+            CONTEXT,
+            CurrencyCode.fromCurrency(AUD_CURRENCY)
+        );
+    }
+
+    @Test
+    public void testFilterMatchesSome2() {
+        this.filterAndCheck(
+            EUR_TEXT,
+            CONTEXT,
+            CurrencyCode.fromCurrency(EUR_CURRENCY)
+        );
+    }
+
+    private void filterAndCheck(final String startsWith,
+                                final CurrencyContext context,
+                                final CurrencyCode... expected) {
+        this.filterAndCheck(
+            startsWith,
+            context,
+            Sets.of(expected)
+        );
+    }
+
+    private void filterAndCheck(final String startsWith,
+                                final CurrencyContext context,
+                                final Set<CurrencyCode> expected) {
+        this.checkEquals(
+            expected,
+            CurrencyCodeSet.filter(
+                startsWith,
+                context
+            )
+        );
+    }
+
+    // TreePrintable....................................................................................................
+
+    @Test
+    public void testTreePrint() {
+        this.treePrintAndCheck(
+            this.createSet(),
+            "AUD\n" +
+                "NZD\n"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<CurrencyCodeSet> type() {
+        return CurrencyCodeSet.class;
+    }
+}
