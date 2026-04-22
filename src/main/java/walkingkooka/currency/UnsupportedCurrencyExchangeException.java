09b4b7be72bb8d30618a231f56e3a6251d41c141
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-22 20:50:18 +1000

    UnsupportedCurrencyExchangeException
    - Closes https://github.com/mP1/walkingkooka-currency/issues/113
    - UnsupportedCurrencyExchange(from, to)

---
+++ src/main/java/walkingkooka/currency/UnsupportedCurrencyExchangeException.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2026 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.currency;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
+
+public final class UnsupportedCurrencyExchangeException extends IllegalArgumentException {
+
+    public static UnsupportedCurrencyExchangeException with(final CurrencyCode from,
+                                                            final CurrencyCode to,
+                                                            final Optional<LocalDateTime> dateTime) {
+        return new UnsupportedCurrencyExchangeException(
+            Objects.requireNonNull(from, "from"),
+            Objects.requireNonNull(to, "to"),
+            Objects.requireNonNull(dateTime, "dateTime")
+        );
+    }
+
+    private UnsupportedCurrencyExchangeException(final CurrencyCode from,
+                                                 final CurrencyCode to,
+                                                 final Optional<LocalDateTime> dateTime) {
+        super("");
+
+        this.from = from;
+        this.to = to;
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String getMessage() {
+        final LocalDateTime dateTime = this.dateTime
+            .orElse(null);
+
+        return "Unsupported currency exchange " + this.from + " to " + this.to +
+            (null != dateTime ?
+                " " + dateTime :
+                ""
+            );
+    }
+
+    public CurrencyCode from() {
+        return this.from;
+    }
+
+    private final CurrencyCode from;
+
+    public CurrencyCode to() {
+        return this.to;
+    }
+
+    private final CurrencyCode to;
+
+    public Optional<LocalDateTime> dateTime() {
+        return this.dateTime;
+    }
+
+    private final Optional<LocalDateTime> dateTime;
+}
---
+++ src/test/java/walkingkooka/currency/UnsupportedCurrencyExchangeExceptionTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2026 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.currency;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.reflect.ThrowableTesting2;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class UnsupportedCurrencyExchangeExceptionTest implements ThrowableTesting2<UnsupportedCurrencyExchangeException> {
+
+    // with.............................................................................................................
+
+    @Test
+    public void testWithNullFromFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> UnsupportedCurrencyExchangeException.with(
+                null,
+                CurrencyCode.parse("NZD"),
+                Optional.empty()
+            )
+        );
+    }
+
+    @Test
+    public void testWithNullToFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> UnsupportedCurrencyExchangeException.with(
+                CurrencyCode.parse("AUD"),
+                null,
+                Optional.empty()
+            )
+        );
+    }
+
+    @Test
+    public void testWithNullDateTimeFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> UnsupportedCurrencyExchangeException.with(
+                CurrencyCode.parse("AUD"),
+                CurrencyCode.parse("NZD"),
+                null
+            )
+        );
+    }
+
+    // getMessage.......................................................................................................
+
+    @Test
+    public void testGetMessage() {
+        this.checkMessage(
+            UnsupportedCurrencyExchangeException.with(
+                CurrencyCode.parse("AUD"),
+                CurrencyCode.parse("NZD"),
+                Optional.empty()
+            ),
+            "Unsupported currency exchange AUD to NZD"
+        );
+    }
+
+    @Test
+    public void testGetMessageWithDateTime() {
+        this.checkMessage(
+            UnsupportedCurrencyExchangeException.with(
+                CurrencyCode.parse("AUD"),
+                CurrencyCode.parse("NZD"),
+                Optional.of(
+                    LocalDateTime.of(
+                        1999,
+                        12,
+                        31,
+                        12,
+                        58,
+                        59
+                    )
+                )
+            ),
+            "Unsupported currency exchange AUD to NZD 1999-12-31T12:58:59"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<UnsupportedCurrencyExchangeException> type() {
+        return UnsupportedCurrencyExchangeException.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+
+    @Override
+    public void testIfClassIsFinalIfAllConstructorsArePrivate() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testAllConstructorsVisibility() {
+        throw new UnsupportedOperationException();
+    }
+}
