1151d801101da34c0fad6e04d9a3991072e5470b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-18 14:44:31 +1000

    https://github.com/mP1/walkingkooka/pull/3326 PublicStaticHelperTesting extends PublicClassTesting
    - https://github.com/mP1/walkingkooka/pull/3326
    - PublicStaticHelperTesting extends PublicClassTesting

--- src/test/java/walkingkooka/currency/CurrencyExchangeRatersTest.java
+++ src/test/java/walkingkooka/currency/CurrencyExchangeRatersTest.java
@@ -17,7 +17,6 @@
 
 package walkingkooka.currency;
 
-import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 
 import java.lang.reflect.Method;
@@ -29,11 +28,6 @@ public final class CurrencyExchangeRatersTest implements PublicStaticHelperTesti
     @Override
     public Class<CurrencyExchangeRaters> type() {
         return CurrencyExchangeRaters.class;
-    }
-
-    @Override
-    public JavaVisibility typeVisibility() {
-        return JavaVisibility.PUBLIC;
     }
 
     @Override
